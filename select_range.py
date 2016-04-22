bf70c41de6943a0c6e58aca57a9f5e339c71c1fd
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-21 23:45:42 -0700

    video output instead

--- select_range.py
+++ select_range.py
@@ -8,6 +8,8 @@ end = 12*60 + 19
 
 cap = cv2.VideoCapture('opendap_hyrax_large_format_RS03ASHS-PN03B-06-CAMHDA301_2016_01_01_CAMHDA301-20160101T000000Z.mov')
 fps = cap.get(cv2.CAP_PROP_FPS)
+w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 print "The video is {} fps".format(fps)
 
 fn = 0
@@ -16,16 +18,21 @@ ncaptured = 0
 # seek to start
 cap.set(cv2.CAP_PROP_POS_MSEC, start*1000)
 
+# video writer
+out = cv2.VideoWriter("selected.mp4", cv2.cv.CV_FOURCC(*'mp4v'), fps, (w, h))
+
 while cap.isOpened() and cap.get(cv2.CAP_PROP_POS_MSEC) < end*1000:
     ret, frame = cap.read()
     if not ret:
         break
 
-    cv2.imwrite('frame_{0}.jpg'.format(fn), frame)
+    #cv2.imwrite('frame_{0}.jpg'.format(fn), frame)
+    out.write(frame)
     ncaptured += 1
 
     fn += 1
 
 
 cap.release()
+out.release()
 print ncaptured
