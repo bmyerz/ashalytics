43b38793e76a88515ee6a0c9452bc875eaf89dc9
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-21 23:48:47 -0700

    fps int

--- select_range.py
+++ select_range.py
@@ -19,8 +19,8 @@ ncaptured = 0
 # seek to start
 cap.set(cv2.CAP_PROP_POS_MSEC, start*1000)
 
-# video writer to same format
-out = cv2.VideoWriter("selected.mp4", codec, fps, (w, h))
+# video writer to same format (slightly different fps?)
+out = cv2.VideoWriter("selected.mp4", codec, int(fps), (w, h))
 
 while cap.isOpened() and cap.get(cv2.CAP_PROP_POS_MSEC) < end*1000:
     ret, frame = cap.read()
