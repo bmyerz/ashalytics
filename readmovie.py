4d593f9234f59961474ad7a2272ab883052c9e70
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-21 22:14:57 -0700

    change grayscale

--- readmovie.py
+++ readmovie.py
@@ -4,10 +4,10 @@ import cv2
 
 cap = cv2.VideoCapture('sample_iTunes.mov')
 
-for i in range(0, 100):
-    if i % 10 == 0:
+for i in range(0, 1000):
+    if i % 100 == 0:
         ret, frame = cap.read()
-        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imwrite('frame_{0}.jpg'.format(i), frame)
 
 
