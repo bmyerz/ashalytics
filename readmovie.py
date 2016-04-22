43a1fbddd1b867da991e5078a1ad7695a47ab4ed
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-21 22:53:12 -0700

    fix my math

--- readmovie.py
+++ readmovie.py
@@ -6,18 +6,22 @@ capture_period_s = 5
 
 cap = cv2.VideoCapture('sample_iTunes.mov')
 fps = cap.get(cv2.CAP_PROP_FPS)
+print "The video is {} fps".format(fps)
 
 fn = 0
+sec = 0
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    if int(float(fn)/fps) % capture_period_s == 0:
+    if float(fn)/fps % capture_period_s == 0:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('frame_{0}.jpg'.format(fn), frame)
+        cv2.imwrite('frame_{0}s.jpg'.format(sec), gray)
+        sec += capture_period_s
 
     fn += 1
 
 
 cap.release()
+print fn
