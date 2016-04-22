02131127c8f43352a2be9209691de58ac45719f4
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-21 22:21:12 -0700

    bug didn't read all frames

--- readmovie.py
+++ readmovie.py
@@ -9,10 +9,14 @@ fps = cap.get(cv2.CAP_PROP_FPS)
 
 fn = 0
 while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
     if int(float(fn)/fps) % capture_period_s == 0:
-        ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imwrite('frame_{0}.jpg'.format(fn), frame)
+
     fn += 1
 
 
